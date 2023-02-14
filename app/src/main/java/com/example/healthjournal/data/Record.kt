a62b208fdd2260ec53915d166fabeb5f8dccf3f2
toha <toha1988cool@gmail.com>
2023-02-14 16:14:34 +0300

    dev in progress

---
+++ app/src/main/java/com/example/healthjournal/data/Record.kt
@@ -0,0 +1,8 @@
+package com.example.healthjournal.data
+
+data class Record(
+    var time:String,
+    var pressure1:Int,
+    var pressure2:Int,
+    var pulse:Int
+)
--- app/src/main/java/com/example/healthjournal/ui/NewRecord.kt
+++ app/src/main/java/com/example/healthjournal/ui/NewRecord.kt
@@ -1,7 +1,22 @@
 package com.example.healthjournal.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.healthjournal.R
+import com.example.healthjournal.databinding.FragmentNewRecordBinding
 
 class NewRecord : Fragment(R.layout.fragment_new_record) {
+
+    private val binding:FragmentNewRecordBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnOk.setOnClickListener {
+
+        }
+
+    }
 }
